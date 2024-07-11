5d00874e5093d2028ae0d2bc2e927b8adf039fa2
Uday Bhatiya <udaybhatiya31@gmail.com>
2024-07-11 15:59:41 +0530

    add email verification route

---
+++ src/app/api/users/verifyemail/route.ts
@@ -0,0 +1,39 @@
+import { connectDB } from "@/dbConfig/dbConfig";
+import User from '@/models/user.model';
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from 'bcryptjs'
+import { sendEmail } from "@/helper/mailer";
+
+connectDB()
+
+export async function POST(request: NextRequest) {
+
+    try {
+
+        const reqBody = await request.json()
+        const {token} = reqBody
+        console.log(token)
+
+        const user = await User.findOne({varifyToken: token, varifyTokenExpiry: {$gt: Date.now()}})
+
+        if (!user) {
+            return NextResponse.json({error: "Invalid token"}, {status: 400})
+        }
+        console.log(user)
+
+        user.isVarified = true
+        user.varifyToken = undefined
+        user.varifyTokenExpiry = undefined
+
+        await user.save()
+
+        return NextResponse.json({
+            message: "Email verified successfully",
+            success: true
+        }, {status: 200})
+        
+    } catch (error :any) {
+        return NextResponse.json({error: error.message}, {status: 500})
+    }
+
+}
